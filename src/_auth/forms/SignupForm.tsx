36c73bac22a5e4edccf34e7db00af49380321df3
Dipto Sarkar <diptosarkarr@gmail.com>
2024-03-07 17:05:40 +0600

    Create Form, Change the globals css and tailwind.config.js file

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,4 +1,5 @@
 import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod"
 import { useForm } from "react-hook-form"
 import { SignupValidation } from "@/lib/validation"
 import { Button } from "@/components/ui/button"
@@ -14,14 +15,14 @@ import {
 import { Input } from "@/components/ui/input"
 
 
- 
-
-
 const SignupForm = () => {
   const form = useForm<z.infer<typeof SignupValidation>>({
     resolver: zodResolver(SignupValidation),
     defaultValues: {
+      name:"",
       username: "",
+      email:"",
+      password:""
     },
   })
  
@@ -33,9 +34,28 @@ const SignupForm = () => {
   }
 
   return (
-    <div>
-      <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+  <Form {...form}>
+
+     <div className="sm:w-420 flex-center flex-col">
+      <img src="/assets/images/logo.svg" />
+      <h3 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h3>
+      <p className="text-light-3 small-medium md:base-regular mt-2">To use snapgram enter your details</p>
+
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4 ">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input type="text" className="shad-input" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
         <FormField
           control={form.control}
           name="username"
@@ -43,19 +63,47 @@ const SignupForm = () => {
             <FormItem>
               <FormLabel>Username</FormLabel>
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <Input type="text" className="shad-input" {...field} />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type="email" className="shad-input" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" className="shad-input" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+
         <Button type="submit">Submit</Button>
       </form>
+      </div>
     </Form>
-    </div>
+    
   )
 }
 
