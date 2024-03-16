d8814e09cdb5c325b59c4e3aa4b3574d4071c4f7
Dipto Sarkar <diptosarkarr@gmail.com>
2024-03-16 12:11:12 +0600

    Initial home page done

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import TopBar from "@/components/ui/shared/TopBar"
+import { LeftSideBar } from "@/components/ui/shared/LeftSideBar"
+import { Outlet } from "react-router-dom"
+import BottomBar from "@/components/ui/shared/BottomBar"
 
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
+    <div className="w-full md:flex">
+    <TopBar/>
+    <LeftSideBar />
+
+    <section className="flex flex-1 h-full">
+    <Outlet/>
+    </section>
+
+    <BottomBar/>
+    </div>
   )
 }
 
