ce4f6387957cbf9b79a246ad0c83bc4844576895
Dipto Sarkar <diptosoftsolution@gmail.com>
2024-03-27 13:39:40 +0600

    Save post and like post functionality done

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,5 +1,5 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
-import { createPost, createUserAccount, getRecentPosts, signInAccount, signOutAccount } from '../appwrite/api'
+import { createPost, createUserAccount, deleteSavedPost, getCurrentUser, getRecentPosts, likePost, savePost, signInAccount, signOutAccount } from '../appwrite/api'
 import { INewPost, INewUser } from '@/types'
 import { QUERY_KEYS } from './queryKeys'
 
@@ -39,5 +39,54 @@ export const useGetRecentPosts = () => {
     queryFn: getRecentPosts
   })
 }
+
+export const useLikePost = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: ({ postId, likesArray }: { postId: string, likesArray: string[] }) => likePost(postId, likesArray),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.GET_POST_BY_ID, data?.$id] })
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.GET_RECENT_POSTS, data?.$id] })
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.GET_POSTS, data?.$id] })
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.GET_CURRENT_USER, data?.$id] })
+    }
+  })
+}
+
+export const useSavePost = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: ({ postId, userId }: { postId: string, userId: string }) => savePost(postId, userId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.GET_RECENT_POSTS] })
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.GET_POSTS] })
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.GET_CURRENT_USER] })
+    }
+  })
+}
+
+export const useDeleteSavedPost = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: (savedRecordId: string) => deleteSavedPost(savedRecordId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.GET_RECENT_POSTS] })
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.GET_POSTS] })
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.GET_CURRENT_USER] })
+    }
+  })
+}
+
+export const useGetCurrentUser = () => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.GET_CURRENT_USER],
+    queryFn: getCurrentUser
+  })
+}
+
+
 
 
